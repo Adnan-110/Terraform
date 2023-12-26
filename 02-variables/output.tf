0564a35155a1181a14e60346a0ef1cff4f9b9f42
Adnan <adnanshaikh11501@gmail.com>
2023-12-26 16:07:34 +0530

    Terraform List Demo

--- 02-variables/output.tf
+++ 02-variables/output.tf
@@ -12,6 +12,7 @@
         value = "The Main Lead of Dunki is ${var.Dunki}"
     }
 
+#   Declaring a Numerical type Variable
     variable "Numerical" {
         default = 1000
     }
@@ -20,3 +21,19 @@
         value = var.Numerical
     }
 
+#   Declaring a List type variable
+
+    variable "Details"{
+        default = [
+            "SRK" ,
+            "Tapsee" ,
+            21-12-2023 ,
+            true
+        ]
+    }
+
+#   Printing List type variable
+
+    output "Dunki-Details" {
+        value = "The Dunki Film is Going to Release on ${var.Details[2]}. The Male Lead of the Film is ${var.Details[0]} and The Female Lead of the Film is ${var.Details[1]} "
+    }       
