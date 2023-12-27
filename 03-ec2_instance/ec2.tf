92bd938c16ffd3c536e8410fddef8ff25c9adc26
Adnan <adnanshaikh11501@gmail.com>
2023-12-28 00:59:05 +0530

    Terraform security groups apply demo

--- 03-ec2_instance/ec2.tf
+++ 03-ec2_instance/ec2.tf
@@ -8,35 +8,7 @@
     }
   }
 
-  output "instance_dns" {
-    value         = aws_instance.web.private_dns              #attribute
-  }
-
-  output "instance_arn" {
-    value         = aws_instance.web.arn                      #attribute
-  }
+ 
 
 
-    resource "aws_security_group" "allow_tls" {
-    name            = "allow_tls"
-    description     = "Allow TLS inbound traffic"
-
-    ingress {
-      description   = "SSH from VPC"
-      from_port     = 22
-      to_port       = 22
-      protocol      = "tcp"
-      cidr_blocks   = ["0.0.0.0/0"]
-    }
-
-    egress {
-      from_port     = 0
-      to_port       = 0
-      protocol      = "-1"
-      cidr_blocks   = ["0.0.0.0/0"]
-    }
-
-    tags            = {
-      Name          = "allow_tls"
-    }
-  }
+    
---
+++ 03-ec2_instance/output.tf
@@ -0,0 +1,7 @@
+ output "instance_dns" {
+    value         = aws_instance.web.private_dns              #attribute
+  }
+
+  output "instance_arn" {
+    value         = aws_instance.web.arn                      #attribute
+  }
