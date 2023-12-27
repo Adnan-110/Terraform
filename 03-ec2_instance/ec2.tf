3cbea796e2831b7c1a03e2cac3c5eefe3f4557c9
Adnan <adnanshaikh11501@gmail.com>
2023-12-28 00:37:21 +0530

    Terraform security groups creation demo

--- 03-ec2_instance/ec2.tf
+++ 03-ec2_instance/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami           = "ami-0f75a13ad2e340a58"           #attribute
+  ami           = "ami-0f75a13ad2e340a58"           #argument
   instance_type = "t2.micro"
 
   tags = {
@@ -13,4 +13,29 @@ output "instance_dns" {
 
 output "instance_arn" {
   value = aws_instance.web.arn                      #attribute
+}
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
