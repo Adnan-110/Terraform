044558918d3771aa8be9e5ee0c69d30f2f45ad8a
Adnan <adnanshaikh11501@gmail.com>
2023-12-28 00:28:07 +0530

    Terraform ec2 instance demo

--- 03-ec2_instance/ec2.tf
+++ 03-ec2_instance/ec2.tf
@@ -1,8 +1,16 @@
 resource "aws_instance" "web" {
-  ami           = "ami-0f75a13ad2e340a58"
+  ami           = "ami-0f75a13ad2e340a58"           #attribute
   instance_type = "t2.micro"
 
   tags = {
     Name = "Demo1"
   }
+}
+
+output "instance_dns" {
+  value = aws_instance.web.private_dns              #attribute
+}
+
+output "instance_arn" {
+  value = aws_instance.web.arn                      #attribute
 }
