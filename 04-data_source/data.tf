a9ff7e74feb367a13f9508cfc5a8b008c794e6fc
Adnan <adnanshaikh11501@gmail.com>
2023-12-28 17:14:25 +0530

    Terraform security groups apply + s3 bucket fro tfstate file demo

--- 04-data_source/data.tf
+++ 04-data_source/data.tf
@@ -1,5 +1,5 @@
 data "aws_ami" "ami" {
-    most_recent      = true
-    name_regex       = "DevOps-LabImage-CentOS7"
-    owners           = ["093842890430"]
+  most_recent      = true
+  name_regex       = "DevOps-LabImage-CentOS7"
+  owners           = ["093842890430"]
 }
