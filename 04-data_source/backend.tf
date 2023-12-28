cdd662e2d3a48be70faac1c34806b175cf368607
Adnan <adnanshaikh11501@gmail.com>
2023-12-28 17:43:40 +0530

    Terraform security groups apply + s3 bucket fro tfstate file demo

--- 04-data_source/backend.tf
+++ 04-data_source/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "ak-tf-state-bucket"
-    key     = "basics/terraform.tfstate"
+    key     = "new/terraform.tfstate"
     region  = "us-east-1"
   }
 } 
